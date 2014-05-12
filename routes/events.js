66abe03ebadcb395243eaa30f2551500f71e5c61
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-05-12 15:31:03 -0400

    ajax post request successful

--- routes/events.js
+++ routes/events.js
@@ -1,3 +1,5 @@
+var Appointment = require('../models/appointment.js')
+
 exports.index = function (req, res){
 	res.render('events/index.ejs');
 }
@@ -11,5 +13,11 @@ exports.new = function(req, res){
 }
 
 exports.create = function(req,res){
-	
+	Appointment.create(req.body.dates, req.body.times, req.session.userId, function(err,status){
+		if(err){
+			res.send({error:err,message:status});
+		} else {
+			res.send({message:status});
+		}
+	});
 }
