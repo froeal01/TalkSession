73017a64b7d048dabf6e85e300f65066e2285946
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-05-15 17:10:22 -0400

    looking decent

--- models/appointment.js
+++ models/appointment.js
@@ -18,34 +18,7 @@ var Appointment = function(args){
 	this.modifiedAt = args.modified_at;
 }
 
-Appointment.create = function(dates,times,creatorId,cb){
-	var errors = new Array()
- pg.connect(config.dbString, function(err,client,done){
- 	if(err){
- 		throw err; //toDo work on handlingerrors//
- 	}
-	dates.forEach(function(date){
- 		times.forEach(function(time){
-     var startTime = /^(\d+:\d+)/.exec(time)
-     var endTime = /(\d+:\d+)$/.exec(time)
-     client.query('INSERT INTO appointments (appointment_date, start_time, end_time, created_by, created_at, modified_at) VALUES($1,$2,$3,$4,to_timestamp($5),to_timestamp($6))',[date,startTime[0],endTime[0],parseInt(creatorId), Date.now(),Date.now()],function(err, results){
-     	if(err){
-     		errors.push(err);
-     	}
-     });
- 			
- 		});
 
-	});
-	done();
-
- });
- if (errors.length > 0){
- 	cb(errors,"failure");
- } else{
- 	cb(null,"success");
- }
-}
 
 
 Appointment.update = function(appointmentId,userId,cb){
@@ -63,7 +36,7 @@ Appointment.update = function(appointmentId,userId,cb){
 
 Appointment.showDailyTimes = function (date, cb){
 	
-	helper.dbCallAllRows("select appointment_id, appointment_date, to_char(start_time,'HH12:MI:SS:am') as start_time, to_char(end_time, 'HH12:MI:SS:am') as end_time from appointments where appointment_date = $1 AND booked = false",[date],Appointment,cb);
+	helper.dbCallAllRows("select appointment_id, appointment_date, to_char(start_time,'HH12:MI:SS:am') as start_time, to_char(end_time, 'HH12:MI:SS:am') as end_time from appointments where appointment_date = $1 AND booked = false ORDER BY start_time asc",[date],Appointment,cb);
 
 }
 
@@ -112,6 +85,34 @@ function confirmedToTrue (appointmentId,callback){
 	helper.dbCall('UPDATE appointments SET(confirmed) = (true) WHERE appointment_id = $1 RETURNING client_email',[parseInt(appointmentId)],function(err,results){
 		callback(err,appointmentId,results.rows[0].client_email)
 	});
+}
+Appointment.create = function(dates,times,creatorId,cb){
+	var errors = new Array()
+ pg.connect(config.dbString, function(err,client,done){
+ 	if(err){
+ 		throw err; //toDo work on handlingerrors//
+ 	}
+	dates.forEach(function(date){
+ 		times.forEach(function(time){
+     var startTime = /^(\d+:\d+)/.exec(time)
+     var endTime = /(\d+:\d+)$/.exec(time)
+     client.query('INSERT INTO appointments (appointment_date, start_time, end_time, created_by, created_at, modified_at) VALUES($1,$2,$3,$4,to_timestamp($5),to_timestamp($6))',[date,startTime[0],endTime[0],parseInt(creatorId), Date.now(),Date.now()],function(err, results){
+     	if(err){
+     		errors.push(err);
+     	}
+     });
+ 			
+ 		});
+
+	});
+	done();
+
+ });
+ if (errors.length > 0){
+ 	cb(errors,"failure");
+ } else{
+ 	cb(null,"success");
+ }
 }
 
 
